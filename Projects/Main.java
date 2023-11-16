ca2daec62a4446fb718b769f8927553e48d8a2d7
Markovwh <131301193+Markovwh@users.noreply.github.com>
2023-11-16 14:06:57 +0200

    Add files via upload

---
+++ Projects/Main.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Main {
+    static int sumOfNumbers(int a, int b) {
+        return a + b;
+    }
+    
+    public static void main(String[] args) {
+        System.out.println(sumOfNumbers(5, 10));
+    } 
+}
+
+    // Scanner scanner = new Scanner(System.in);
+    // String a = scanner.nextLine();
+    // String b = scanner.nextLine();
+    // int num1 = Integer.parseInt(a);
+    // int num2= Integer.parseInt(b);
+
+    // int num_a = Integer.valueOf(a);
+    // int num_b = Integer.valueOf(b);
