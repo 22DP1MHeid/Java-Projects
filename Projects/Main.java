e23746793de3b4b9d415d299bf2dcaa198d0f2bc
22DP1MHeid <131352976+22DP1MHeid@users.noreply.github.com>
2024-01-24 19:13:51 +0000

    added five java exercises to the java main

--- Projects/Main.java
+++ Projects/Main.java
@@ -1,20 +1,54 @@
 import java.util.Scanner;
 
 public class Main {
-    static int sumOfNumbers(int a, int b) {
-        return a + b;
-    }
-    
     public static void main(String[] args) {
-        System.out.println(sumOfNumbers(5, 10));
-    } 
-}
 
-    // Scanner scanner = new Scanner(System.in);
-    // String a = scanner.nextLine();
-    // String b = scanner.nextLine();
-    // int num1 = Integer.parseInt(a);
-    // int num2= Integer.parseInt(b);
 
-    // int num_a = Integer.valueOf(a);
-    // int num_b = Integer.valueOf(b);
+    // 1. Prints "Hello along with the user's name (String)
+    System.out.println("Hello");
+    System.out.println("Markuss Heideamanis");
+    System.out.println();
+    
+
+    // 2. Prints the sum of two numbers (int)
+    int sum = 74 + 36;
+    System.out.println(sum);
+    System.out.println();
+    
+    
+    // 3. Prints the division of two numbers (int)
+    int division = 50 / 3;
+    System.out.println(division);
+    System.out.println();
+
+
+    // 4. Prints multiple mathematical results (int)
+    int a = -5 + 8 * 6;
+    int b = (55 + 9) % 9;
+    int c = 20 + -3 * 5 / 8;
+    int d = 5 + 15 / 3 * 2 - 8 % 3;
+
+    System.out.println(a);
+    System.out.println(b);
+    System.out.println(c);
+    System.out.println(d);
+    System.out.println();
+
+
+    // 5. Takes two user inputed numbers and prints their multiplication (int)
+    Scanner scanner = new Scanner(System.in);
+
+    System.out.println("Enter the first number: ");
+    int num1 = Integer.parseInt(scanner.nextLine());
+
+    System.out.println("Enter the second number: ");
+    int num2 = Integer.parseInt(scanner.nextLine());
+
+    int multiplication = num1 * num2;
+    System.out.println(multiplication);
+
+
+    // 6.
+
+    }
+}    
