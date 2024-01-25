44df49e8709fd0faf388cd1796dda3b5bc04dffb
22DP1MHeid <131352976+22DP1MHeid@users.noreply.github.com>
2024-01-25 17:10:31 +0000

    Completed exercises till the 16th

--- Projects/Main.java
+++ Projects/Main.java
@@ -48,6 +48,7 @@ public class Main {
     System.out.println(multiplication);
     System.out.println();
 
+
     // 6. Takes two user inputed numbers and prints multiple operation results
     System.out.print("Enter the first number: ");
     num1 = Integer.parseInt(scanner.nextLine());
@@ -68,6 +69,7 @@ public class Main {
     System.out.println(remainder);
     System.out.println();
 
+
     // 7. Takes an user inputed number and prints it multiplication table up to 10
     System.out.print("Enter a number: ");
     num1 = Integer.parseInt(scanner.nextLine());
@@ -77,6 +79,94 @@ public class Main {
     }
     System.out.println();
 
-    // 8. 
+
+    // 8. Outputs "JAVA" made form ASCII symbols
+    System.out.println("   J     a   v     v  a");
+    System.out.println("   J    a a   v   v  a a");
+    System.out.println("J  J   aaaaa   V V  aaaaa");
+    System.out.println(" JJ   a     a   V  a     a");
+    System.out.println();
+
+
+    // 9. Calculates a complex math problem and outputs a double
+    double problem = ((25.5 * 3.5 - 3.5 * 3.5) / (40.5 - 4.5));
+    System.out.println(problem);
+    System.out.println();
+
+
+    // 10. Calculates a complex formula and outputs a double
+    problem = 4.0 * (1 - (1.0 / 3) + (1.0 / 5) - (1.0 / 7) + (1.0 / 9) - (1.0 / 11));
+    System.out.println(problem);
+    System.out.println();
+
+
+    // 11. Using a radius program calculates and outputs the perimeter and area of a circle
+    double circleRadius = 7.5;
+    double circlePerimeter = 2 * Math.PI * circleRadius;
+    double circleArea = Math.PI * Math.pow(circleRadius, 2);
+
+    System.out.println("Perimeter is = " + circlePerimeter);
+    System.out.println("Area is = " + circleArea);
+    System.out.println();
+
+
+    // 12. Takes three user inputed numbers and prints the average
+    System.out.print("Enter the first number: ");
+    num1 = Integer.parseInt(scanner.nextLine());
+
+    System.out.print("Enter the second number: ");
+    num2 = Integer.parseInt(scanner.nextLine());
+
+    System.out.print("Enter the third number: ");
+    int num3 = Integer.parseInt(scanner.nextLine());
+
+    System.out.println((num1 + num2 + num3) / 3);
+    System.out.println();
+
+
+    // 13. Prints the area and perimeter of a rectangle
+    double width = 5.5;
+    double height = 8.5;
+    double rectangleArea = width * height;
+    double rectanglePerimeter = width * 2 + height * 2;
+
+    System.out.println("Area is " + width + " * " + height + " = " + rectangleArea);
+    System.out.println("Perimeter is 2 * (" + width + " + " + height + ") = " + rectanglePerimeter);
+    System.out.println();
+    
+
+    // 14. Outputs the American flag with ASCII symbols
+    System.out.println("* * * * * * ==================================");
+    System.out.println(" * * * * *  ==================================");
+    System.out.println("* * * * * * ==================================");
+    System.out.println(" * * * * *  ==================================");
+    System.out.println("* * * * * * ==================================");
+    System.out.println(" * * * * *  ==================================");
+    System.out.println("* * * * * * ==================================");
+    System.out.println(" * * * * *  ==================================");
+    System.out.println("* * * * * * ==================================");
+    System.out.println("==============================================");
+    System.out.println("==============================================");
+    System.out.println("==============================================");
+    System.out.println("==============================================");
+    System.out.println("==============================================");
+    System.out.println("==============================================");
+    System.out.println();
+
+
+    // 15. Swaps two variables
+    a = 10;
+    b = 5;
+
+    int temp = a;
+    a = b;
+    b = temp;
+
+    System.out.println(a);
+    System.out.println(b);
+    System.out.println();
+
+
+    // 16. 
     }
 }    
